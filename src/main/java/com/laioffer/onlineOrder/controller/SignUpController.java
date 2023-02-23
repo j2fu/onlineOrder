9a61bba09af1d6027f0b2d8724605e20329bbbed
j2fu <j2fu@ucsd.edu>
2023-02-22 21:07:53 -0800

    set up 1.1

--- src/main/java/com/laioffer/onlineOrder/controller/SignUpController.java
+++ src/main/java/com/laioffer/onlineOrder/controller/SignUpController.java
@@ -1,5 +1,9 @@
 package com.laioffer.onlineOrder.controller;
+
+
 import com.laioffer.onlineOrder.entity.Customer;
+import com.laioffer.onlineOrder.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,12 +11,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+
 @Controller
 public class SignUpController {
-    @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    @ResponseStatus(value = HttpStatus.CREATED)
-    public void signup(@RequestBody Customer customer){
 
+
+    private CustomerService customerService;
+
+
+    @Autowired
+    public SignUpController(CustomerService customerService) {
+        this.customerService = customerService;
     }
 
+
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public void signUp(@RequestBody Customer customer) {
+        customerService.signUp(customer);
+    }
 }
